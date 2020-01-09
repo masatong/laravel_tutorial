cc585069258094355b0de71aeca3ee05052298a9
Masato Nagata <masato.naganaga1@gmail.com>
2020-01-09 10:00:45 +0000

    機能追加
    フォルダ新規作成機能追加
    フォルダ名入力フォームのバリデーション機能追加

--- routes/web.php
+++ routes/web.php
@@ -1,3 +1,8 @@
 <?php
 
+// 選択されたフォルダに紐づいたタスクの表示
 Route::get('/folders/{id}/tasks', 'TaskController@index')->name('tasks.index');
+// フォルダ作成ページ表示
+Route::get('/folders/create', 'FolderController@showCreateForm')->name('folders.create');
+// フォルダ作成処理の実行
+Route::post('/folders/create', 'FolderController@create');
